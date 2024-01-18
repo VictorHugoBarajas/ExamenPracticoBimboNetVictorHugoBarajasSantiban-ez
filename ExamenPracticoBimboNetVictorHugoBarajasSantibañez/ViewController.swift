58f5cda310b632d5b896d89acc6d6eb1015007af
Victor Hugo Barajas Santibañez <comodindelarge@gmail.com>
2024-01-18 12:53:49 -0600

    Initial Commit

---
+++ ExamenPracticoBimboNetVictorHugoBarajasSantibañez/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamenPracticoBimboNetVictorHugoBarajasSantibañez
+//
+//  Created by Victor Hugo Barajas Santibañez on 18/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
